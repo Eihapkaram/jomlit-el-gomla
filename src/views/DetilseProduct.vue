725c2fd65f9c14452c5e651cb7fb30d63c1d76bb
Eihapkaram <eihapkaram9090@gmail.com>
2025-11-13 06:25:08 -0800

    Update DetilseProduct.vue
    ب

--- src/views/DetilseProduct.vue
+++ src/views/DetilseProduct.vue
@@ -407,9 +407,6 @@ export default {
   },
 
   async mounted() {
-    if (this.SingleProduct?.images?.length > 0) {
-    this.tab = this.SingleProduct.images[0];
-  };
     await this.getSingle(this.$route.params.idparam);
     await this.getReviwes(this.$route.params.idparam);
     setTimeout(() => {}, 100);
